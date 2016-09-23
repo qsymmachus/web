c8a388916b3e7eb432a35a34715108d02a3e1d87
John Olmsted <john@olmsted.io>
2016-09-23 10:11:32 -0700

    Middleman now minifies CSS

--- config.rb
+++ config.rb
@@ -16,12 +16,6 @@ page '/*.txt', layout: false
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
-# General configuration
-
-###
-# Helpers
-###
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
@@ -31,9 +25,5 @@ page '/*.txt', layout: false
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+   activate :minify_css
 end
