c38bc665b8fd48a3532bed58fc56ff5345f30cd2
John Olmsted <john@olmsted.io>
2016-11-24 12:36:56 -0600

    Remove cruft from config.rb

--- config.rb
+++ config.rb
@@ -9,13 +9,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
